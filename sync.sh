8b2c99909708dab588afcaa590c7ea5b17809a0b
Rosen Velikov <rosen.velikov@clouway.com>
2022-08-13 12:58:17 +0300

    sync update

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm  ./brouter.conf
+rm ./conf/bng-vt-1/brouter.conf
+rm ./conf/bng-vt-2/brouter.conf
 git pull
-./set_brouter.conf.sh
-cp ./brouter.conf /etc/bisonrouter/brouter.conf
+../set_brouter.conf.sh
+cp ./conf/$HOSTNAME/brouter.conf /etc/bisonrouter/brouter.conf
