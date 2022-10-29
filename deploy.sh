04a175f11d6ceaf1dc08a04413f373009ba438a4
Rosen Velikov <rosen.velikov@clouway.com>
2022-10-29 13:53:07 +0300

    alpha v0.1

--- deploy.sh
+++ deploy.sh
@@ -1,47 +1,30 @@
-# bisonRouter
-
-
-VRRP Setup
-add "net.ipv4.ip_nonlocal_bind=1" 
-sudo nano /etc/sysctl.conf
-```
-net.ipv4.ip_nonlocal_bind=1
-```
-run sudo sysctl -p
-sudo apt-get install keepalived
-sudo nano /etc/keepalived/keepalived.conf
-
-sudo service keepalived start
-sudo systemctl status keepalived
-sudo service keepalived stop
-
-# install template parser
-
-mkdir renderizer && cd renderizer && wget https://github.com/gomatic/renderizer/releases/download/v2.0.13/renderizer_linux_x86_64.tar.gz && tar zxvf renderizer_linux_x86_64.tar.gz && sudo mv ./renderizer /usr/bin/. && cd ..
-
-```
 #!/bin/bash
+
+if [[ ! -f "./renderizer" ]]; then
+  wget https://github.com/gomatic/renderizer/releases/download/v2.0.13/renderizer_linux_x86_64.tar.gz && tar zxvf renderizer_linux_x86_64.tar.gz renderizer && rm renderizer_linux_x86_64.tar.gz
+fi
+
+
 ts=$(date '+%Y%m%dT%H%M%S')
 cp /etc/bisonrouter/brouter.conf /etc/bisonrouter/brouter.conf.$ts
 cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.$ts
 cp /etc/keepalived/keepalived.conf /etc/keepalived/keepalived.conf.$ts
 
-
-if ! renderizer ./tmpl/brouter.conf --settings=brouter.yaml --master=true --missing zero > /etc/bisonrouter/brouter.conf
+if ! ./renderizer ./tmpl/brouter.conf --settings=brouter.yaml --master=true --missing zero > /etc/bisonrouter/brouter.conf
 then
     echo error in /tmpl/brouter.conf
     echo Rolling back configuration
     cp /etc/bisonrouter/brouter.conf.$ts /etc/bisonrouter/brouter.conf
     cp /etc/netplan/00-installer-config.yaml.$ts /etc/netplan/00-installer-config.yaml
     cp /etc/keepalived/keepalived.conf.$ts /etc/keepalived/keepalived.conf
-elseif ! renderizer ./tmpl/00-installer-config.yaml --settings=brouter.yaml --master=true --missing zero > /etc/netplan/00-installer-config.yaml
+elseif ! ./renderizer ./tmpl/00-installer-config.yaml --settings=brouter.yaml --master=true --missing zero > /etc/netplan/00-installer-config.yaml
 then 
     echo error in /tmpl/00-installer-config.yaml
     echo Rolling back configuration
     cp /etc/bisonrouter/brouter.conf.$ts /etc/bisonrouter/brouter.conf
     cp /etc/netplan/00-installer-config.yaml.$ts /etc/netplan/00-installer-config.yaml
     cp /etc/keepalived/keepalived.conf.$ts /etc/keepalived/keepalived.conf
-elseif ! ./tmpl/keepalived.conf --settings=brouter.yaml --master=true --missing zero > /etc/keepalived/keepalived.conf
+elseif ! ./renderizer ./tmpl/keepalived.conf --settings=brouter.yaml --master=true --missing zero > /etc/keepalived/keepalived.conf
 then 
     echo error in /tmpl/keepalived.conf
     echo Rolling back configuration
@@ -49,5 +32,3 @@ then
     cp /etc/netplan/00-installer-config.yaml.$ts /etc/netplan/00-installer-config.yaml
     cp /etc/keepalived/keepalived.conf.$ts /etc/keepalived/keepalived.conf
 fi
-```
-
--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm ./conf/bng-vt-1/brouter.conf
-rm ./conf/bng-vt-2/brouter.conf
+# rm ./conf/bng-vt-1/brouter.conf
+# rm ./conf/bng-vt-2/brouter.conf
 git pull
-../set_brouter.conf.sh
-cp ./conf/$HOSTNAME/brouter.conf /etc/bisonrouter/brouter.conf
+# ../set_brouter.conf.sh
+# cp ./conf/$HOSTNAME/brouter.conf /etc/bisonrouter/brouter.conf
